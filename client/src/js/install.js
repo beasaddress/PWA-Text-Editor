21d2a0cdaae65f542e320cea45a4342315027530
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-24 17:54:07 -0700

    added conditional rendering to navBar

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,28 +3,20 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
-});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-
-    if (!promptEvent) {
-     return;
-    }
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+    
   
-    
-    promptEvent.prompt();
-    
-    
-    window.deferredPrompt = null;
-    
-    butInstall.classList.toggle('hidden', true);
-});
+    installBtn.addEventListener('click', () => {
+      event.prompt();
+      installBtn.setAttribute('disabled', true);
+      installBtn.textContent = 'Installed!';
+    });
+  });
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+     
+  console.log('👍', 'appinstalled', event);
 });
